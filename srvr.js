f7ec987a20e2956acb5e531d827d81a1c0af8719
Rehaan Khan <rehaancool796@gmail.com>
2019-07-27 22:23:39 +0530

    Update srvr.js

--- srvr.js
+++ srvr.js
@@ -8,30 +8,7 @@ var creds = require('./client_secret.json');
 
 var doc = new GoogleSpreadsheet('1JIsAksQUxAFvwuUdOa_n9LDTulOgnkOQPo2_nMi85uQ');
 
-let ggf = ''
 
-doc.useServiceAccountAuth(creds, function (err) {
-
- 
-
-    // Get all of the rows from the spreadsheet.
-  
-    doc.getRows(1, function (err, rows) {
-  
-  const info = rows.forEach((index,item) => {
-    //  console.log(`Candidate name is ${index.name}, and belongs to ${index.city}`)
-
-    
- ggf = ggf + `<h1>Candidate name is ${index.name}, and belongs to ${index.city}</h1>`
-
-     
-
-  })
-      //console.log(rows[0].name)
-  
-    });
-  
-  });
 
 
 
@@ -95,6 +72,59 @@ app.get('/create', function(req, res) {
 app.get('/', (req,res) => {
     
 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ let ggf = ''
+
+doc.useServiceAccountAuth(creds, function (err) {
+
+ 
+
+    // Get all of the rows from the spreadsheet.
+  
+    doc.getRows(1, function (err, rows) {
+  
+  const info = rows.forEach((index,item) => {
+    //  console.log(`Candidate name is ${index.name}, and belongs to ${index.city}`)
+
+    
+ ggf = ggf + `<h1>Candidate name is ${index.name}, and belongs to ${index.city}</h1>`
+
+     
+
+  })
+      //console.log(rows[0].name)
+  
+    });
+  
+  });
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
     
   res.render('home.hbs', {
       pageTitle: ggf 
